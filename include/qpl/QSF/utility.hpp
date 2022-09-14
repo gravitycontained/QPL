93b43fb62cb641ed271acc7c570c10fbe3edc7aa
Rabl Daniel <d.rabl@ita.local>
2022-09-14 09:49:17 +0200

    added qsf::camera_cursor_lock

--- include/qpl/QSF/utility.hpp
+++ include/qpl/QSF/utility.hpp
@@ -8,6 +8,10 @@
 #include <qpl/qpldeclspec.hpp>
 #include <qpl/vardef.hpp>
 #include <qpl/type_traits.hpp>
+
+#include <qpl/camera.hpp>
+#include <qpl/QSF/framework.hpp>
+
 #include <SFML/Graphics/RenderStates.hpp>
 #include <SFML/Graphics.hpp>
 #include <qpl/vector.hpp>
@@ -34,6 +38,56 @@ namespace qsf {
 	}
 
 	QPLDLL void run_exception_window(const std::string& exception, std::string font_path, qpl::vector2i dimension = { 1280, 720 });
+
+	struct camera_cursor_lock {
+		bool lock = false;
+		bool first = true;
+
+		template<typename T> requires (qpl::is_derived<T, qsf::base_state>())
+		void cursor_on(qpl::camera& camera, T& state) {
+			state.show_cursor();
+			camera.allow_looking = false;
+			this->lock = false;
+		}
+		template<typename T> requires (qpl::is_derived<T, qsf::base_state>())
+		void cursor_off(qpl::camera& camera, T& state) {
+			state.hide_cursor();
+			camera.allow_looking = true;
+			this->lock = true;
+
+			this->set_cursor_center(state);
+		}
+
+		
+		template<typename T> requires (qpl::is_derived<T, qsf::base_state>())
+		void set_cursor_center(T& state) {
+			state.set_cursor_position(state.center());
+		}
+
+		template<typename T> requires (qpl::is_derived<T, qsf::base_state>())
+		void reset(qpl::camera& camera, T& state) {
+			this->cursor_off(camera, state);
+		}
+
+		template<typename T> requires (qpl::is_derived<T, qsf::base_state>())
+		void update(qpl::camera& camera, T& state) {
+			if (this->first) {
+				this->reset(camera, state);
+				this->first = false;
+			}
+
+			if (state.has_gained_focus()) {
+				this->cursor_off(camera, state);
+			}
+			if (state.has_lost_focus()) {
+				this->cursor_on(camera, state);
+			}
+
+			if (this->lock) {
+				this->set_cursor_center(state);
+			}
+		}
+	};
 }
 
 #endif
